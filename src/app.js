405b683cab9203b8b4ebe73d13f1727f64248fe4
AlejandroGonzalezG <ale.gonzalez.gut@gmail.com>
2022-06-24 21:51:49 +0000

    proyecto domain generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//import "bootstrap";
+//import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
+
+let pronombre = ["el", "ese", "tu"];
+let adjetivo = ["hediondo", "largo", "hermoso"];
+let sustantivo = ["perrito", "gato", "cooper", "arbol"];
+let dominio = [".com", ".cl", ".net"];
 
 window.onload = function() {
   //write your code here
+  for (let i = 0; i < pronombre.length; i++) {
+    for (let j = 0; j < sustantivo.length; j++) {
+      for (let h = 0; h < adjetivo.length; h++) {
+        for (let k = 0; k < dominio.length; k++) {
+          console.log(pronombre[i] + sustantivo[j] + adjetivo[h] + dominio[k]);
+        }
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
 };
